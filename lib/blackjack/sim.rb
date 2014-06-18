3f92613dc671d8c378761d90a91c37d4e5cd0028
Michael Batchelor <batchelor@nationbuilder.com>
2014-06-17 18:43:50 -0700

    Initial commit

---
+++ lib/blackjack/sim.rb
@@ -0,0 +1,7 @@
+require "blackjack/sim/version"
+
+module Blackjack
+  module Sim
+    # Your code goes here...
+  end
+end
---
+++ lib/blackjack/sim/version.rb
@@ -0,0 +1,5 @@
+module Blackjack
+  module Sim
+    VERSION = "0.0.1"
+  end
+end
